918eb0c76e433ee5e2b5d045b9f7529bce0427d3
Song Khoi <songkhoi123@gmail.com>
2025-02-24 01:53:18 +0700

    using concurrent

---
+++ .metals/.reports/metals-full/2025-02-24/r_empty-definition_(root)_01-26-26-031.md
@@ -0,0 +1,44 @@
+error id: views/html/
+file:///C:/Users/Khoi%20Nguyen/Desktop/scalalala/app/controllers/HomeController.scala
+empty definition using pc, found symbol in pc: views/html/
+semanticdb not found
+|empty definition using fallback
+non-local guesses:
+	 -
+
+Document text:
+
+```scala
+package controllers
+
+import java.util.Date
+import java.text.SimpleDateFormat
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/** This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
+
+  /** Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method will be
+    * called when the application receives a `GET` request with a path of `/`.
+    */
+  def index() = Action {
+    val date = new Date()
+    val dateStr = new SimpleDateFormat().format(date)
+
+    implicit request: Request[AnyContent] => Ok(views.html.index())
+  }
+}
+
+```
+
+#### Short summary: 
+
+empty definition using pc, found symbol in pc: views/html/
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,22 @@
 package controllers
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)(
+    implicit ec: ExecutionContext
+) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action.async { implicit request: Request[AnyContent] =>
+    Future {
+      val now = ZonedDateTime.now()
+      val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+      val dateStr = now.format(formatter)
+      Ok(views.html.index(dateStr))
+    }
   }
 }
